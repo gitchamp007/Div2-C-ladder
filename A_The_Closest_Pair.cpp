147c12b76ef5b87be74bbff743fef8246c99d39b
Raj Singh <rajsingh@kgpian.iitkgp.ac.in>
2024-05-19 00:19:16 +0530

    summation of series compared with k

---
+++ A_The_Closest_Pair.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+#define ll long long 
+#define ul unsigned long long 
+#define ld long double 
+#define fl(i,start,end) for(ll i=start;i<end;i++)
+#define M 1000000007
+#define endl '\n' 
+using namespace std; 
+
+void solve() {
+    ll n, k; cin >> n >> k;
+    if(n*(n-1)/2 > k){
+        for(int i = 0; i < n; i++) cout << 0 << " " << i << endl;
+    } else cout << "no solution";
+}
+
+signed main() {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    // int _t; cin >> _t; while(_t--) 
+        solve();
+    return 0;
+}
